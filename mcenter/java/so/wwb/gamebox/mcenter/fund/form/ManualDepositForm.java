c798fba48a850cf22619fecdb099b71f463e6cd0
cherry <cherry1012@mail.com>
2017-10-21 09:24:26 +0800

    ==先暂时还原人工存取变更代码

--- mcenter/java/so/wwb/gamebox/mcenter/fund/form/ManualDepositForm.java
+++ mcenter/java/so/wwb/gamebox/mcenter/fund/form/ManualDepositForm.java
@@ -2,13 +2,13 @@ package so.wwb.gamebox.mcenter.fund.form;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
+import org.soul.commons.lang.string.StringTool;
 import org.soul.commons.query.enums.Operator;
 import org.soul.commons.validation.form.constraints.Depends;
 import org.soul.commons.validation.form.support.Comment;
 import org.soul.web.support.IForm;
 import so.wwb.gamebox.mcenter.common.consts.FormValidRegExps;
 
-import javax.validation.constraints.Digits;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Pattern;
 
@@ -19,10 +19,9 @@ import javax.validation.constraints.Pattern;
 public class ManualDepositForm implements IForm {
     private String $userNames;
     private String result_rechargeAmount;
+    private String auditType;
     private String auditMultiple;
     private String result_checkRemark;
-    private String favorable_favorableTotalAmount;
-    private String favorable_auditFavorableMultiple;
 
     @NotBlank(message = "fund.ManualDepositForm.userNames.notBlank")
     @Pattern(message = "fund.ManualDepositForm.userNames.Pattern", regexp = FormValidRegExps.ENGLISH_NUMBER_COMMA)
@@ -45,9 +44,18 @@ public class ManualDepositForm implements IForm {
         this.result_rechargeAmount = result_rechargeAmount;
     }
 
+    @NotBlank
+    public String getAuditType() {
+        return auditType;
+    }
+
+    public void setAuditType(String auditType) {
+        this.auditType = auditType;
+    }
+
     @Depends(message = "fund.ManualDepositForm.auditMultiple.notBlank", property = {"auditType"}, value = {"0"}, operator = {Operator.NE}, jsValueExp = "$(\"input[name=auditType]:checked\").val()")
     @Pattern(regexp = FormValidRegExps.MONEY, message = "fund.ManualDepositForm.auditMultiple.Pattern")
-    @Max(value = 100, message = "fund.ManualDepositForm.auditMultiple.Pattern")
+    @Max(value = 100,message = "fund.ManualDepositForm.auditMultiple.Pattern")
     public String getAuditMultiple() {
         return auditMultiple;
     }
@@ -63,24 +71,5 @@ public class ManualDepositForm implements IForm {
 
     public void setResult_checkRemark(String result_checkRemark) {
         this.result_checkRemark = result_checkRemark;
-    }
-
-    @Pattern(regexp = FormValidRegExps.MONEY, message = "fund_auto.金额格式不正确")
-    public String getFavorable_favorableTotalAmount() {
-        return favorable_favorableTotalAmount;
-    }
-
-    public void setFavorable_favorableTotalAmount(String favorable_favorableTotalAmount) {
-        this.favorable_favorableTotalAmount = favorable_favorableTotalAmount;
-    }
-
-    @Digits(integer = 3,fraction = 2)
-    @Max(value = 100)
-    public String getFavorable_auditFavorableMultiple() {
-        return favorable_auditFavorableMultiple;
-    }
-
-    public void setFavorable_auditFavorableMultiple(String favorable_auditFavorableMultiple) {
-        this.favorable_auditFavorableMultiple = favorable_auditFavorableMultiple;
     }
 }
