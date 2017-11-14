e5f8c5b19b36a9b12a47f0b3bfe07a3e86208c3e
rambo <rambo@dawoo.com>
2017-11-14 21:09:07 +0800

    赔率范围大小设置修改

--- mcenter/java/so/wwb/gamebox/mcenter/lottery/form/SiteLotteryOddsForm.java
+++ mcenter/java/so/wwb/gamebox/mcenter/lottery/form/SiteLotteryOddsForm.java
@@ -18,7 +18,7 @@ public class SiteLotteryOddsForm implements IForm {
     private String[] lotteryOdds$$_rebate;
 
     @NotBlank(message = "common.赔率不能为空")
-//    @Min(message = "赔率不能小于1", value = 1)
+    @DecimalMin("0.001")
     @Pattern(message = "格式错误", regexp = "^[0-9]\\d*(\\.\\d*[1-9]{1,3})?$")
     @Digits(integer = 7,fraction = 3)
     public String[] getLotteryOdds$$_odd() {
@@ -30,6 +30,7 @@ public class SiteLotteryOddsForm implements IForm {
     }
 
     @NotBlank(message = "common.赔率不能为空")
+    @DecimalMin("0.001")
     @Pattern(message = "格式错误", regexp = "^[0-9]\\d*(\\.\\d*[1-9]{1,3})?$")
     @Digits(integer = 1,fraction = 3)
     public String[] getLotteryOdds$$_rebate() {
