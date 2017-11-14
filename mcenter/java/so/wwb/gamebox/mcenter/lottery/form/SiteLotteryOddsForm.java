4fd85e36154e00ac113434de0280f4ddd47eb493
rambo <rambo@dawoo.com>
2017-11-14 18:08:58 +0800

    添加审计日志日志

--- mcenter/java/so/wwb/gamebox/mcenter/lottery/form/SiteLotteryOddsForm.java
+++ mcenter/java/so/wwb/gamebox/mcenter/lottery/form/SiteLotteryOddsForm.java
@@ -18,8 +18,8 @@ public class SiteLotteryOddsForm implements IForm {
     private String[] lotteryOdds$$_rebate;
 
     @NotBlank(message = "common.赔率不能为空")
-    @Min(message = "赔率不能小于1", value = 1)
-    @Pattern(message = "格式错误", regexp = "^[0-9]\\d*(\\.\\d{1,3})?$")
+//    @Min(message = "赔率不能小于1", value = 1)
+    @Pattern(message = "格式错误", regexp = "^[0-9]\\d*(\\.\\d*[1-9]{1,3})?$")
     @Digits(integer = 7,fraction = 3)
     public String[] getLotteryOdds$$_odd() {
         return lotteryOdds$$_odd;
@@ -30,7 +30,7 @@ public class SiteLotteryOddsForm implements IForm {
     }
 
     @NotBlank(message = "common.赔率不能为空")
-    @Pattern(message = "格式错误", regexp = "^[0-9]\\d*(\\.\\d{1,3})?$")
+    @Pattern(message = "格式错误", regexp = "^[0-9]\\d*(\\.\\d*[1-9]{1,3})?$")
     @Digits(integer = 1,fraction = 3)
     public String[] getLotteryOdds$$_rebate() {
         return lotteryOdds$$_rebate;
