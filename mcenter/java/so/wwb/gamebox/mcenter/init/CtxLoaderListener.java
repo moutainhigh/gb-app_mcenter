095680bb4916258d2ed72837c971da471f3da0b4
tony <tony@wwb.so>
2017-11-15 19:20:37 +0800

    优化Loader的代码

--- mcenter/java/so/wwb/gamebox/mcenter/init/CtxLoaderListener.java
+++ mcenter/java/so/wwb/gamebox/mcenter/init/CtxLoaderListener.java
@@ -1,20 +1,11 @@
 package so.wwb.gamebox.mcenter.init;
 
-import org.soul.web.init.BaseCtxLoaderListener;
 import so.wwb.gamebox.web.init.CommonCtxLoaderListener;
 
-import javax.servlet.ServletContextEvent;
-import java.util.TimeZone;
 
 /**
  * Created by Kevice on 2015/3/23 0023.`
  */
 public class CtxLoaderListener extends CommonCtxLoaderListener {
 
-    @Override
-    public void contextInitialized(ServletContextEvent event) {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC")); // 设置JVM默认时区为０时区
-        super.contextInitialized(event);
-        BaseCtxLoaderListener.isStoped=false;
-    }
 }
