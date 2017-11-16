06a6daa9466ff7896912b3261ae42d9591037b1d
leo <leo@dawoo.com>
2017-11-16 15:13:41 +0800

    充值记录表查询改为视图查询

---
+++ mcenter/java/so/wwb/gamebox/mcenter/setting/form/VCreditRecordSearchForm.java
@@ -0,0 +1,20 @@
+package so.wwb.gamebox.boss.credit.form;
+
+import org.soul.web.support.IForm;
+
+
+/**
+ * 充值记录查询视图查询表单验证对象
+ *
+ * @author leo
+ * @time 2017-11-15 20:19:43
+ */
+//region your codes 1
+public class VCreditRecordSearchForm implements IForm {
+//endregion your codes 1
+
+    //region your codes 2
+
+    //endregion your codes 2
+
+}
