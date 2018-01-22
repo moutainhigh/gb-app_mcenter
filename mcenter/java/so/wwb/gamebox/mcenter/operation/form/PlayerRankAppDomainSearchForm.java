41fa12ab5da9d2ac85369a897d2cf1df1395f1b6
back <back@dawoo.com>
2018-01-22 14:24:38 +0800

    系统，站点参数，根据层级设置下载域名

---
+++ mcenter/java/so/wwb/gamebox/mcenter/operation/form/PlayerRankAppDomainSearchForm.java
@@ -0,0 +1,20 @@
+package so.wwb.gamebox.mcenter.operation.form;
+
+import org.soul.web.support.IForm;
+
+
+/**
+ * APP下载域名层级关联表查询表单验证对象
+ *
+ * @author back
+ * @time 2018-1-18 19:49:09
+ */
+//region your codes 1
+public class PlayerRankAppDomainSearchForm implements IForm {
+//endregion your codes 1
+
+    //region your codes 2
+
+    //endregion your codes 2
+
+}
