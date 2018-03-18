aab728cc2ad1c58bb5914a8628bba024b3c82278
steffan <steffan@dawoo.com>
2018-03-18 14:36:36 +0800

    活动效果监控:新增

---
+++ mcenter/java/so/wwb/gamebox/mcenter/operation/form/VActivityMonitorForm.java
@@ -0,0 +1,20 @@
+package so.wwb.gamebox.mcenter.operation.form;
+
+import org.soul.web.support.IForm;
+
+
+/**
+ * 活动效果监控视图表单验证对象
+ *
+ * @author steffan
+ * @time 2018-3-18 11:13:42
+ */
+//region your codes 1
+public class VActivityMonitorForm implements IForm {
+//endregion your codes 1
+
+    //region your codes 2
+
+    //endregion your codes 2
+
+}
