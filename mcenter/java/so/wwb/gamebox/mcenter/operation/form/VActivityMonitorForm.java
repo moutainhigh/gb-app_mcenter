29e71b68770ccd9a0d7f117234bfa42cf6c6be0d
steffan <steffan@dawoo.com>
2018-03-21 11:15:21 +0800

    活动效果监控页面查询

--- mcenter/java/so/wwb/gamebox/mcenter/operation/form/VActivityMonitorForm.java
+++ mcenter/java/so/wwb/gamebox/mcenter/operation/form/VActivityMonitorForm.java
@@ -1,6 +1,9 @@
 package so.wwb.gamebox.mcenter.operation.form;
 
 import org.soul.web.support.IForm;
+import so.wwb.gamebox.mcenter.common.consts.FormValidRegExps;
+
+import javax.validation.constraints.Pattern;
 
 
 /**
@@ -14,7 +17,16 @@ public class VActivityMonitorForm implements IForm {
 //endregion your codes 1
 
     //region your codes 2
+    private String search_ipApplyStr;
 
+    @Pattern(regexp = FormValidRegExps.IP)
+    public String getSearch_ipApplyStr() {
+        return search_ipApplyStr;
+    }
+
+    public void setSearch_ipApplyStr(String search_ipApplyStr) {
+        this.search_ipApplyStr = search_ipApplyStr;
+    }
     //endregion your codes 2
 
 }
