8db5309dd2efc8e60898794c84992bc5e14b1be9
martin <martin@mail.com>
2018-04-12 20:44:01 +0800

    捷报数据中心需求.

---
+++ mreport/WebContent/daily/RealTimeSummary.jsp
@@ -0,0 +1,26 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: martin
+  Date: 18-4-12
+  Time: 上午10:53
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ include file="/include/include.inc.jsp" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <%@ include file="/include/include.head.jsp" %>
+    <title>整体走势-实时统计</title>
+</head>
+<body>
+<h1>实时统计图表</h1>
+</body>
+</html>
+<script src="${resRoot}/js/daily/RealTimeSummary.js" type="text/javascript" charset="utf-8"></script>
+<script src="${resRoot}/js/Popper.js" type="text/javascript" charset="utf-8"></script>
+<script src="${resRoot}/js/echarts.common.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="${resRoot}/js/swiper.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="${resRoot}/js/jqPaginator.js" type="text/javascript" charset="utf-8"></script>
+<script src="${resRoot}/js/bootstrap-datetimepicker.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="https://gw.alipayobjects.com/os/antv/assets/g2/3.0.5-beta.5/g2.min.js" type="text/javascript"></script>
---
+++ mreport/java/so/wwb/gamebox/mcenter/analyze/daily/controller/SiteDailyController.java
@@ -0,0 +1,75 @@
+package so.wwb.gamebox.mcenter.analyze.daily.controller;
+
+import org.soul.web.controller.BaseIndexController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 站点日常数据
+ * @author martin
+ * Created by martin on 18-4-12.
+ */
+@Controller
+@RequestMapping("/daily")
+public class SiteDailyController extends BaseIndexController {
+
+    private static final String OPERATION_TRENDS = "/daily/OperationTrends";
+
+    private static final String REAL_TIME_SUMMARY = "/daily/RealTimeSummary";
+
+    private static final String ACTIVE_PLAYER = "/daily/ActivePlayer";
+
+    private static final String NEW_ADDED_PLAYER = "/daily/NewAddedPlayer";
+
+    private static final String PLAYER_RETAIN = "/daily/PlayerRetain";
+
+    /**
+     * 经营趋势
+     * @return
+     */
+    @RequestMapping("/operationTrends")
+    public String operationTrends(HttpServletRequest request, Model model) {
+        return OPERATION_TRENDS;
+    }
+
+    /**
+     * 实时总览
+     * @return
+     */
+    @RequestMapping("/realTimeSummary")
+    public String realTimeSummaryData(HttpServletRequest request, Model model) {
+        return REAL_TIME_SUMMARY;
+    }
+
+    /**
+     * 活跃玩家
+     * @return
+     */
+    @RequestMapping("/activePlayer")
+    public String activePlayer(HttpServletRequest request, Model model) {
+        return ACTIVE_PLAYER;
+    }
+
+    /**
+     * 玩家留存
+     * @return
+     */
+    @RequestMapping("/playerRetain")
+    public String playerRetain(HttpServletRequest request, Model model) {
+        return PLAYER_RETAIN;
+    }
+
+    /**
+     * 新增玩家
+     * @return
+     */
+    @RequestMapping("/newAddedPlayer")
+    public String newAddedPlayer(HttpServletRequest request, Model model) {
+        return NEW_ADDED_PLAYER;
+    }
+}
