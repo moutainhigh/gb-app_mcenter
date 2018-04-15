d62381fed533adde63c048e2f78d0778ad208993
gavin <gavin@dawoo.com>
2018-04-15 15:32:00 +0800

    实时总览 jsp

--- mreport/java/so/wwb/gamebox/mcenter/analyze/daily/controller/SiteDailyController.java
+++ mreport/java/so/wwb/gamebox/mcenter/analyze/daily/controller/SiteDailyController.java
@@ -1,13 +1,19 @@
 package so.wwb.gamebox.mcenter.analyze.daily.controller;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.fileupload.util.LimitedInputStream;
+import org.soul.commons.data.json.JsonTool;
 import org.soul.web.controller.BaseIndexController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import so.wwb.gamebox.model.site.report.po.RealtimeProfile;
+import so.wwb.gamebox.model.site.report.vo.RealtimeProfileListVo;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.FieldPosition;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * 站点日常数据
@@ -43,6 +49,31 @@ public class SiteDailyController extends BaseIndexController {
      */
     @RequestMapping("/realTimeSummary")
     public String realTimeSummaryData(HttpServletRequest request, Model model) {
+        List<RealtimeProfile> profileLists = new ArrayList<>();
+        Date date = new Date();
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//        String format = dateFormat.format(date);
+        for(int i = 1;i<=10;i++){
+            RealtimeProfile realtimeProfile = new RealtimeProfile();
+            realtimeProfile.setRealtimeDate(date);
+            realtimeProfile.setVisitorAll(10);
+            realtimeProfile.setActiveAll(30);
+            realtimeProfile.setDepositAll(20);
+            realtimeProfile.setEffcTransactionAll(40);
+            realtimeProfile.setOnlineAll(50);
+            realtimeProfile.setProfitAll(60);
+            realtimeProfile.setRegisterAll(70);
+            profileLists.add(realtimeProfile);
+        }
+
+        for(int i = 0;i<=23;i++){
+            RealtimeProfile realtimeProfile = new RealtimeProfile();
+//            realtimeProfile
+        }
+        RealtimeProfileListVo realtimeProfileListVo = new RealtimeProfileListVo();
+        realtimeProfileListVo.setResult(profileLists);
+        model.addAttribute("command",realtimeProfileListVo);
+        model.addAttribute("realtimeProfileListJson", JsonTool.toJson(profileLists));
         return REAL_TIME_SUMMARY;
     }
 
