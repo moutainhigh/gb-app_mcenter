834e20787ddf73226cbc79db2f53411846f5df59
martin <martin@mail.com>
2018-04-22 18:02:16 +0800

    捷报数据中心需求:运营日常统计图表开发.

--- mreport-service/java/so/wwb/gamebox/service/site/report/OperationSummaryService.java
+++ mreport-service/java/so/wwb/gamebox/service/site/report/OperationSummaryService.java
@@ -1,19 +1,30 @@
 package so.wwb.gamebox.service.site.report;
 
+import org.soul.commons.collections.CollectionTool;
+import org.soul.commons.lang.DateTool;
 import org.soul.service.support.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import so.wwb.gamebox.data.site.report.OperationSummaryMapper;
 import so.wwb.gamebox.iservice.site.report.IOperationSummaryService;
 import so.wwb.gamebox.model.site.report.po.OperationSummary;
+import so.wwb.gamebox.model.site.report.vo.OperationSummaryChartVo;
 import so.wwb.gamebox.model.site.report.vo.OperationSummaryListVo;
 import so.wwb.gamebox.model.site.report.vo.OperationSummaryVo;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * 运营日常统计服务
+ *
  * @author martin
  * @time 2018-4-17
  */
 public class OperationSummaryService extends BaseService<OperationSummaryMapper, OperationSummaryListVo, OperationSummaryVo, OperationSummary, Integer> implements IOperationSummaryService {
+
+    private static final String DAY1 = "yesterday";
+
+    private static final String DAY2 = "beforeOfYesterday";
 
     @Autowired
     private OperationSummaryMapper summaryMapper;
@@ -21,32 +32,27 @@ public class OperationSummaryService extends BaseService<OperationSummaryMapper,
     @Override
     public OperationSummaryVo getOperationSummaryData(OperationSummaryVo condition) {
         OperationSummaryVo result = new OperationSummaryVo();
-        OperationSummary summary1 = new OperationSummary();
-        summary1.setTitle("存款金额");
-        summary1.setNumerical(8000D);
-        result.getEntities().add(summary1);
-
-        OperationSummary summary2 = new OperationSummary();
-        summary2.setTitle("取现金额");
-        summary2.setNumerical(6000D);
-        result.getEntities().add(summary2);
-
-        //计算占比
-        summary1.setPercent(summary1.getNumerical()/(summary1.getNumerical()+summary2.getNumerical()));
-        summary2.setPercent(summary2.getNumerical()/(summary1.getNumerical()+summary2.getNumerical()));
+        List<OperationSummary> list = summaryMapper.getOperationSummaryOfDays();
+        if (CollectionTool.isNotEmpty(list)) {
+            generateBalanceBarChartData(result, list);
+            generateEffectiveBarChartData(result, list);
+        }
+        result.getEntities().addAll(list);
         return result;
     }
 
     /**
      * 存取差额统计
+     *
      * @return
      */
-    public OperationSummaryVo differenceSummary() {
+    public List<OperationSummary> differenceSummary() {
         return null;
     }
 
     /**
      * 有效投注统计
+     *
      * @return
      */
     public OperationSummaryVo effectiveTradeSummary() {
@@ -101,4 +107,83 @@ public class OperationSummaryService extends BaseService<OperationSummaryMapper,
         return null;
     }
 
+    /**
+     * 取现差额玉珏图数据
+     * @param result
+     * @param last
+     * @return
+     */
+    protected void generateBalanceBarChartData(OperationSummaryVo result, List<OperationSummary> list) {
+        OperationSummary summary = getWhichDayData(list, this.DAY1);
+        if (summary != null) {
+            OperationSummaryChartVo item1 = new OperationSummaryChartVo();
+            item1.setNumerical(summary.getDepositAmount());
+            item1.setTips(getDay(summary.getStaticDate()));
+            item1.setTitle("存款金额");
+            result.getBalanceBarChart().add(item1);
+
+            OperationSummaryChartVo item2 = new OperationSummaryChartVo();
+            item2.setNumerical(summary.getWithdrawalAmount());
+            item2.setTips(getDay(summary.getStaticDate()));
+            item2.setTitle("取现金额");
+            result.getBalanceBarChart().add(item2);
+
+            //计算占比
+            item1.setPercent(item1.getNumerical() / (item1.getNumerical() + item2.getNumerical()));
+            item2.setPercent(item2.getNumerical() / (item1.getNumerical() + item2.getNumerical()));
+        }
+    }
+
+    /**
+     * 有效投注额玉珏图数据
+     * @param result
+     * @param last
+     * @return
+     */
+    protected void generateEffectiveBarChartData(OperationSummaryVo result, List<OperationSummary> list) {
+        OperationSummary summary1 = getWhichDayData(list, DAY1);
+        OperationSummary summary2 = getWhichDayData(list, DAY2);
+        if (summary1 != null && summary2 != null) {
+            OperationSummaryChartVo item1 = new OperationSummaryChartVo();
+            item1.setNumerical(summary1.getEffectiveTransactionAll());
+            item1.setTitle(getDay(summary1.getStaticDate()));
+            item1.setTips("有效投注额");
+            result.getEffectiveBarChart().add(item1);
+
+            OperationSummaryChartVo item2 = new OperationSummaryChartVo();
+            item2.setNumerical(summary2.getEffectiveTransactionAll());
+            item2.setTitle(getDay(summary2.getStaticDate()));
+            item2.setTips("有效投注额");
+            result.getEffectiveBarChart().add(item2);
+
+            //计算占比
+            item1.setPercent(item1.getNumerical() / (item1.getNumerical() + item2.getNumerical()));
+            item2.setPercent(item2.getNumerical() / (item1.getNumerical() + item2.getNumerical()));
+        }
+    }
+
+    /**
+     * 获取指定周期的运营统计数据
+     * @param list
+     * @param flag
+     * @return
+     */
+    protected OperationSummary getWhichDayData(List<OperationSummary> list, String flag) {
+        for (OperationSummary item : list) {
+            if (this.DAY1.equalsIgnoreCase(flag)
+                    && DateTool.daysBetween(new Date(), item.getStaticDate()) == 1) {
+                return item;
+            }
+            if (this.DAY2.equalsIgnoreCase(flag)
+                    && DateTool.daysBetween(new Date(), item.getStaticDate()) == 2) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    private String getDay(Date date) {
+        String str = DateTool.formatDate(date, DateTool.yyyy_MM_dd);
+        return str.substring(str.indexOf("-")+1, str.length()).replace("-", ".");
+    }
 }
