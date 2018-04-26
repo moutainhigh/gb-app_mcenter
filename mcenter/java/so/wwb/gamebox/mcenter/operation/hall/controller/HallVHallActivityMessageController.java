c2af98c76d060705942ca7584143d97a336a85e5
steffan <steffan@dawoo.com>
2018-04-26 14:48:08 +0800

    活动大厅>   活动编辑,唯一展示限制                      列表: 状态展示

--- mcenter/java/so/wwb/gamebox/mcenter/operation/hall/controller/HallVHallActivityMessageController.java
+++ mcenter/java/so/wwb/gamebox/mcenter/operation/hall/controller/HallVHallActivityMessageController.java
@@ -419,7 +419,7 @@ public class HallVHallActivityMessageController extends HallActivityController<I
             assignment(activityTypeVo, vActivityMessageVo);
 
 
-            boolean success = ServiceActivityTool.vActivityMessageService().activityRelease(vActivityMessageVo);
+            boolean success = ServiceActivityTool.vHallActivityMessageService().activityRelease(vActivityMessageVo);
 
             if (success) {
                 updateSiteContentAudit();
