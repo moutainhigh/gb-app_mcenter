71544ccbebc31fc71d14c507316252d7bd88aabd
linsen <linsen@me.so>
2018-07-09 21:11:09 +0800

    存款审核添加站内信

--- mcenter/java/so/wwb/gamebox/mcenter/fund/controller/BaseDepositController.java
+++ mcenter/java/so/wwb/gamebox/mcenter/fund/controller/BaseDepositController.java
@@ -7,15 +7,20 @@ import org.soul.commons.collections.MapTool;
 import org.soul.commons.currency.CurrencyTool;
 import org.soul.commons.data.json.JsonTool;
 import org.soul.commons.dict.DictTool;
+import org.soul.commons.init.context.CommonContext;
 import org.soul.commons.lang.DateTool;
 import org.soul.commons.lang.string.I18nTool;
 import org.soul.commons.lang.string.StringTool;
+import org.soul.commons.locale.LocaleDateTool;
 import org.soul.commons.locale.LocaleTool;
 import org.soul.commons.log.Log;
 import org.soul.commons.net.IpTool;
 import org.soul.commons.query.Paging;
 import org.soul.commons.support._Module;
 import org.soul.model.listop.po.SysListOperator;
+import org.soul.model.msg.notice.vo.NoticeVo;
+import org.soul.model.security.privilege.po.SysUser;
+import org.soul.model.security.privilege.vo.SysUserVo;
 import org.soul.model.sys.po.SysDict;
 import org.soul.model.sys.po.SysParam;
 import org.soul.model.sys.vo.SysParamVo;
@@ -27,6 +32,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import so.wwb.gamebox.common.dubbo.ServiceActivityTool;
 import so.wwb.gamebox.common.dubbo.ServiceSiteTool;
+import so.wwb.gamebox.common.dubbo.ServiceTool;
 import so.wwb.gamebox.iservice.master.fund.IVPlayerDepositService;
 import so.wwb.gamebox.mcenter.enmus.ListOpEnum;
 import so.wwb.gamebox.mcenter.fund.form.DepositRemarkForm;
@@ -36,8 +42,14 @@ import so.wwb.gamebox.mcenter.session.SessionManager;
 import so.wwb.gamebox.model.*;
 import so.wwb.gamebox.model.bitcoin.vo.PoloniexOrderResult;
 import so.wwb.gamebox.model.boss.enums.TemplateCodeEnum;
+import so.wwb.gamebox.model.common.MessageI18nConst;
+import so.wwb.gamebox.model.common.notice.enums.AutoNoticeEvent;
+import so.wwb.gamebox.model.common.notice.enums.NoticeParamEnum;
 import so.wwb.gamebox.model.company.setting.po.SysCurrency;
 import so.wwb.gamebox.model.company.site.po.SiteCurrency;
+import so.wwb.gamebox.model.company.site.po.SiteCustomerService;
+import so.wwb.gamebox.model.company.sys.po.SysSite;
+import so.wwb.gamebox.model.company.sys.vo.SysSiteVo;
 import so.wwb.gamebox.model.currency.po.CurrencyRate;
 import so.wwb.gamebox.model.enums.UserTypeEnum;
 import so.wwb.gamebox.model.listop.FilterRow;
@@ -656,6 +668,8 @@ public abstract class BaseDepositController extends BaseCrudController<IVPlayerD
         }
         // 最终状态
         putState(vo, map);
+        //发送站内信
+        checkResult(vo);
         return map;
     }
 
@@ -742,4 +756,128 @@ public abstract class BaseDepositController extends BaseCrudController<IVPlayerD
 ////        ServiceTool.messageService().sendToMcenterMsg(message);
 //    }
 
+    /**
+     * 审核结果处理
+     */
+    private void checkResult(PlayerRechargeVo vo) {
+        PlayerRecharge recharge = vo.getResult();
+        if (vo.isSuccess()) {
+            // 审核成功
+            if (RechargeStatusEnum.SUCCESS.getCode().equals(recharge.getRechargeStatus())
+                    || RechargeStatusEnum.ONLINE_SUCCESS.getCode().equals(recharge.getRechargeStatus())) {
+                NoticeVo noticeVo = NoticeVo.autoNotify(AutoNoticeEvent.DEPOSIT_AUDIT_SUCCESS, recharge.getPlayerId());
+                publishMessage(noticeVo, recharge);
+            } else if ((RechargeStatusEnum.FAIL.getCode().equals(recharge.getRechargeStatus()) || RechargeStatusEnum.ONLINE_FAIL.getCode().equals(recharge.getRechargeStatus())) && StringTool.isNotBlank(vo.getGroupCode())) {
+                // 审核失败
+                NoticeVo noticeVo = NoticeVo.manualNotify(vo.getGroupCode(), null, recharge.getPlayerId());
+                publishMessage(noticeVo, recharge);
+            }
+        }
+    }
+
+    /**
+     * 发布消息
+     */
+    private void publishMessage(NoticeVo noticeVo, PlayerRecharge recharge) {
+        try {
+            noticeVo = addParams(recharge, noticeVo);
+            ServiceTool.noticeService().publish(noticeVo);
+        } catch (Exception ex) {
+            LOG.error(ex, "存款审核发布消息不成功");
+        }
+    }
+
+    /**
+     * 设置信息模板参数
+     */
+    private NoticeVo addParams(PlayerRecharge recharge, NoticeVo noticeVo) {
+        // 查询用户信息
+        SysUser sysUser = getSysUser(recharge.getPlayerId());
+
+        String timeZone = sysUser.getDefaultTimezone();
+        if (timeZone == null) {
+            SysSite site = getSysSite();
+            if (site != null) {
+                timeZone = site.getTimezone();
+            } else {
+                LOG.error("未设置时区，返回参数为空，发送的站内信将无法替换参数");
+                return noticeVo;
+            }
+        }
+
+        // 初始化消息参数
+        initNoticeParam(recharge, noticeVo, sysUser, timeZone);
+        return noticeVo;
+    }
+
+    /**
+     * 查询玩家
+     */
+    private SysUser getSysUser(Integer playerId) {
+        SysUserVo sysUserVo = new SysUserVo();
+        sysUserVo.getSearch().setId(playerId);
+        SysUser user = ServiceTool.sysUserService().get(sysUserVo).getResult();
+        return user == null ? new SysUser() : user;
+    }
+
+    /**
+     * 获取站点信息
+     */
+    private SysSite getSysSite() {
+        SysSiteVo siteVo = new SysSiteVo();
+        siteVo.getSearch().setId(SessionManager.getSiteId());
+        siteVo = ServiceTool.sysSiteService().get(siteVo);
+        return siteVo.getResult();
+    }
+
+    /**
+     * 初始化消息参数
+     */
+    private void initNoticeParam(PlayerRecharge recharge, NoticeVo noticeVo, SysUser sysUser, String timeZone) {
+        SysCurrency sysCurrency = Cache.getSysCurrency().get(sysUser.getDefaultCurrency());
+        Date createTime = recharge.getCreateTime();
+        Pair<String, String> orderLaunchTime = new Pair(NoticeParamEnum.ORDER_LAUNCH_TIME.getCode(),
+                LocaleDateTool.formatDate(createTime, CommonContext.getDateFormat().getDAY_SECOND(), timeZone));
+        Pair<String, String> orderAmount = new Pair<>(NoticeParamEnum.ORDER_AMOUNT.getCode(), sysCurrency.getCurrencySign() + CurrencyTool.formatCurrency(recharge.getRechargeAmount()));
+        Pair<String, String> orderNum = new Pair<>(NoticeParamEnum.ORDER_NUM.getCode(), recharge.getTransactionNo());
+        Pair<String, String> siteName = new Pair(NoticeParamEnum.SITE_NAME.getCode(), SessionManager.getSiteName(null));
+        Pair<String, String> year = new Pair(NoticeParamEnum.YEAR.getCode(),
+                LocaleDateTool.formatDate(createTime, CommonContext.getDateFormat().getYEAR(), timeZone));
+        Pair<String, String> month = new Pair(NoticeParamEnum.MONTH.getCode(),
+                LocaleDateTool.formatDate(createTime, CommonContext.getDateFormat().getMONTH(), timeZone));
+        Pair<String, String> day = new Pair<>(NoticeParamEnum.DAY.getCode(), getDay(timeZone));
+        Pair<String, String> user = new Pair(NoticeParamEnum.USER.getCode(),
+                LocaleDateTool.formatDate(createTime, CommonContext.getDateFormat().getDAY(), sysUser.getUsername()));
+        Pair<String, String> customer = getCustomer();
+        noticeVo.addParams(orderLaunchTime, orderAmount, orderNum, siteName, year, month, day, user, customer);
+    }
+
+    /**
+     * 获取当前日期（几号）
+     */
+    private String getDay(String timeZone) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.setTimeZone(TimeZone.getTimeZone(timeZone));
+        return String.valueOf(cal.get(Calendar.DAY_OF_MONTH));
+    }
+
+    /**
+     * 客服设置
+     */
+    private Pair<String, String> getCustomer() {
+        Pair<String, String> customer;
+        SiteCustomerService siteCustomerService = Cache.getDefaultCustomerService();
+        if (siteCustomerService != null) {
+            String url = siteCustomerService.getParameter();
+            if (StringTool.isNotBlank(url) && !url.contains("http")) {
+                url = "http://" + url;
+            }
+            customer = new Pair<>(NoticeParamEnum.CUSTOMER.getCode(), "<a class=\"\" href=\"" + url
+                    + "\" target=\"_blank\">" + LocaleTool.tranMessage(_Module.COMMON, "contactCustomerService") + "</a>");
+        } else {
+            customer = new Pair<>(NoticeParamEnum.CUSTOMER.getCode(), LocaleTool.tranMessage(_Module.COMMON, "contactCustomerService"));
+        }
+        return customer;
+    }
 }
